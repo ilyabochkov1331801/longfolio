12f9d354b13304ff76db280afc7e454baeb8ed2b
Ilya Bochkov <ilyabochkovib@gmail.com>
2026-03-11 20:36:48 +0100

    Initial Commit

---
+++ longfolio/longfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  longfolioApp.swift
+//  longfolio
+//
+//  Created by Илья Бочков on 11.03.26.
+//
+
+import SwiftUI
+
+@main
+struct longfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
